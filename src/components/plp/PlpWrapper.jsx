540d161ae4fcb64724790c9fad510cfea27f7833
yakkad <yakkad@sqli.com>
2023-06-19 10:31:37 +0100

    Product Listing Page : components & styles

---
+++ src/components/plp/PlpWrapper.jsx
@@ -0,0 +1,12 @@
+import FilterSection from "./FilterSection";
+import Plp from "./Plp";
+
+const PlpWrapper = () => {
+  return (
+    <>
+      <FilterSection />
+      <Plp />
+    </>
+  );
+};
+export default PlpWrapper;
